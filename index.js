e1bd99c35e6e9e9193cc9de78373ac7e31023e89
patriciapavia <patricia.pavia@terrapages.com>
2021-03-29 17:32:37 +1100

    initial commit"

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const redis = require("redis");
+
+const PORT = process.env.PORT || 5000;
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+const client = redis.createClient(REDIS_PORT);
+
+const app = express();
+
+async function getRepos(req, res, next) {
+  try {
+    console.log("Fetching Data....");
+    const { username } = req.params;
+
+    const response = await fetch(`https://api.github.com/users/${username}`);
+
+    const data = await response.json();
+    console.log(data);
+
+    const repos = data.public_repos;
+
+    // set data to redis
+
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+  }
+}
+
+app.get("/repos/:username", getRepos);
+
+app.listen(5000, () => {
+  console.log(`app listening on port ${PORT}`);
+});
