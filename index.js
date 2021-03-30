c830921c882821e84392aefd865a106b405e825e
patriciapavia <patricia.pavia@terrapages.com>
2021-03-30 16:41:54 +1100

    created redis cache

--- index.js
+++ index.js
@@ -3,35 +3,57 @@ const fetch = require("node-fetch");
 const redis = require("redis");
 
 const PORT = process.env.PORT || 5000;
-const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const REDIS_PORT = process.env.PORT_REDIS || 6379;
 
 const client = redis.createClient(REDIS_PORT);
 
 const app = express();
 
+// Set response
+function setResponse(username, repos) {
+  return `<h2>${username} has ${repos} Github repos</h2>`;
+}
+
+// Make request to Github for data
 async function getRepos(req, res, next) {
   try {
-    console.log("Fetching Data....");
+    console.log("Fetching Data...");
+
     const { username } = req.params;
 
     const response = await fetch(`https://api.github.com/users/${username}`);
 
     const data = await response.json();
-    console.log(data);
 
     const repos = data.public_repos;
 
-    // set data to redis
+    // Set data to Redis
+    client.setex(username, 3600, repos);
 
-    res.send(data);
-  } catch (error) {
-    console.log(error);
+    res.send(setResponse(username, repos));
+  } catch (err) {
+    console.error(err);
     res.status(500);
   }
 }
 
-app.get("/repos/:username", getRepos);
+// Cache middleware
+function cache(req, res, next) {
+  const { username } = req.params;
+
+  client.get(username, (err, data) => {
+    if (err) throw err;
+
+    if (data !== null) {
+      res.send(setResponse(username, data));
+    } else {
+      next();
+    }
+  });
+}
+
+app.get("/repos/:username", cache, getRepos);
 
 app.listen(5000, () => {
-  console.log(`app listening on port ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 });
